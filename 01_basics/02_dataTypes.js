b33b66d578cd882cd009225f21cdea2493fc82c6
Md Shadab Irfan <mdshadabirfan@gmail.com>
2024-12-03 22:53:40 +0000

    Datatype class

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; //Treat all JS code as newer version
+
+// alert(3 + 3); => This will not be executed as we are using nodejs not browser
+
+console.log(3 
+    + 3) //=> We should avoid this type of code as our focus on code readability should be high
+
+let name = "Shadab";
+let age = 22;
+let isLoggedIn = true;
+
+
+//number => range => 2 raised to the power of 53
+//bigint
+//string => "" => anything under double/single quotes
+//boolean => true/false
+//null => standalone value => if nothing is stored
+//undefined => variable that has not been assigned
+//symbol => used for uniqueness
+
+
+//object
+
+console.log(typeof "Shadab");
+console.log(typeof null); //=> null is of OBJECT DataType
+console.log(typeof undefined); //=> undefined is of undefined DataType
