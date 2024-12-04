94e797e38250f98d068ee877a5281afba1018ce3
Md Shadab Irfan <mdshadabirfan@gmail.com>
2024-12-04 21:58:42 +0000

    More Conversion Operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -39,3 +39,24 @@ console.log(typeof stringSomeNumber);
 
 
 
+// ****************** Operations **********************
+let value = 3;
+let negValue = -value;
+console.log(negValue);
+
+console.log(2 + 2);
+console.log(2 - 2);
+console.log(2 * 2);
+console.log(2 ** 3);
+console.log(2 / 2);
+console.log(2 % 3);
+
+
+let str1 = "Hello";
+let str2 = " World!";
+let str3 = str1 + str2;
+console.log(str3);
+
+
+
+
