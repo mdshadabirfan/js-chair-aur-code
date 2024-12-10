c5f7ed34e64373a3236b83029796706d35d79d29
Md Shadab Irfan <mdshadabirfan@gmail.com>
2024-12-10 00:42:24 +0000

    Objects Intro in Depth

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -1,7 +1,9 @@
-//Singleton => object.create
+// ---Object can hold any type of Data-----
+
+//Singleton => object.create =>This will create a singleton object
 
 //Object literal => Object can hold any type of Data
-const mySymbol = Symbol();//Declaring a symbol => Symbol must be declared outside of the object
+const mySymbol = Symbol("symbolKey");//Declaring a symbol => Symbol must be declared outside of the object
 const userDetails ={
     name: "Shadab", //all keys are stored in the form of string
     email: "shadab@yahoo.com",
@@ -15,8 +17,8 @@ const userDetails ={
 console.log(userDetails);//This will print the whole object userDetails
 
 /*Accessing the object */
-console.log(userDetails.age);//Accessing like this includes just dot and key
-console.log(userDetails["location"]);//Accessing like this include ["anyKey"]
+console.log(userDetails.age);//Accessing like this includes just dot and the key
+console.log(userDetails["location"]);//Accessing like this include ["keyName"]
 
 console.log(userDetails["college name"]);//This is better method than dot access
 
@@ -27,9 +29,22 @@ userDetails.email = "shadab@dragon_ball.com";//This will overwrite the existing 
 console.log(userDetails["email"]);
 
 /* Value Freezing */
-Object.freeze(userDetails);
-userDetails.email = "shadab@pokemon.com"
+// Object.freeze(userDetails);//This will freeze the object
+// userDetails.email = "shadab@pokemon.com"//Email will not updated as the object is freezed
+// console.log(userDetails);//This will print the original object although we have tried to change the email
+
+//Creating a new key value pair in object userDetails
+userDetails.isBeginner = true;
 console.log(userDetails);
+
+//Creating a function adding it to the object as a key-value pair
+userDetails.greeting = function(){
+    return `Hello JS user, ${this["name"]}`; //We can use this keyword to access the object=> THIS is referring to the userDetails object 
+}
+console.log(userDetails["greeting"]);//This will print function (anonymous)=> The function is reference not executed
+
+console.log(userDetails.greeting());//This will execute the function stored in greeting key
+
 
 
 
